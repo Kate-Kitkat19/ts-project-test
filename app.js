aad7941aeae693bc9756764d451bf47bf07eaf47
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-02-08 12:40:15 +0200

    module 2

--- app.js
+++ app.js
@@ -7,5 +7,58 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
-
-
+var age;
+var name1;
+var toggle;
+var empty;
+var notInitialize;
+var callback;
+callback = function (a) {
+    return 100 + a;
+};
+var anything;
+anything = -20;
+anything = "Text";
+anything = {};
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+    console.log("str", str);
+}
+var person;
+var Progress;
+(function (Progress) {
+    Progress[Progress["LOADING"] = 0] = "LOADING";
+    Progress[Progress["READY"] = 1] = "READY";
+})(Progress || (Progress = {}));
+var somevalue;
+var isAble;
+/////////
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error("Error");
+}
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: "2021-01-01",
+        updateAt: "2021-05-01"
+    }
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close"
+};
+console.log(page1, page2);
--- app.ts
+++ app.ts
@@ -9,3 +9,82 @@ function add(num1: number, num2: number) {
 button.addEventListener("click", function () {
   console.log(add(+input1.value, +input2.value));
 });
+
+let age: number;
+let name1: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback: (a: any) => number;
+callback = (a) => {
+  return 100 + a;
+};
+
+let anything: any;
+anything = -20;
+anything = "Text";
+anything = {};
+
+let some: unknown;
+some = "Text";
+
+let str: string;
+
+if (typeof some === "string") {
+  str = some;
+  console.log("str", str);
+}
+
+let person: [string, number];
+
+enum Progress {
+  LOADING,
+  READY,
+}
+
+let somevalue: string | number;
+let isAble: "enable" | "disable";
+
+/////////
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: string;
+    updateAt: string;
+  };
+};
+
+const page1: Page = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2: Page = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
+
+console.log(page1, page2);
